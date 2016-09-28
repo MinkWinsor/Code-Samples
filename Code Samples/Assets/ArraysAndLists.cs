d09a7f3ebfcd1fb8fef0f4787a646f6394467f85
MinkWinsor <steven.olsen18@gmail.com>
2016-09-28 00:17:47 -0600

    Scope
    added scope

---
+++ Code Samples/Assets/ArraysAndLists.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/*Arrays and lists are used to store collections of variables or objects.
+ * They are useful because if we use an array we don't need 100+ variables being tracked at once, and we can store larger amounts of data easier.
+ * Arrays and lists use index values to refer to each 'element' inside the array or list.
+ * */
+public class ArraysAndLists : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //Arrays are declared like variables, but include [] after the data type. They must also be set to an instance of an array using the new keyword, before or after they are created.
+
+        string[] bottlesOfBeerOnTheWall = new string[99];
+
+        //Another way to declare an array is to automatically fill it with values. The size of the array will be set to the values.
+        //Values are put inside {} with commas.
+        string[] pokemonGames = { "Red", "Blue", "Yellow", "Gold", "Silver", "Crystal"};
+
+        //To reference an element, simply use the array name with [index] after. Index is an integer, starting at 0.
+        print(pokemonGames[2]); //Prints the third element.
+
+        //Arrays have a set value. If you need to change the number of elements in an array, a new array is often needed. You can also reassign old arrays.
+        bottlesOfBeerOnTheWall = new string[98]; //DELETES CONTENTS OF ARRAY.
+
+        //Lists are similar to arrays, but with added features. They don't have a set size, and so the size can be changed on the fly.
+        //Lists also have functions to insert values, sort values, and more.
+        //Lists are made in a different way from arrays, and can hold more types of objects.
+        //the library System.Collections.Generic is used for lists.
+
+        List<string> redShirts = new List<string>();
+
+        //This list can have elements added, which will automatically increase the size of the list.
+        redShirts.Add("Marko");
+        redShirts.Add("Mr. Tibbs");
+        redShirts.Add("Guy");
+
+        print("There are " + redShirts.Count + " Red shirts");
+
+        foreach (string red in redShirts)
+        {
+            print(red + " has died.");
+            
+        }
+
+        redShirts.Clear();
+        print("There are " + redShirts.Count + " Red shirts");
+    }
+	
+}
---
+++ Code Samples/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour {
+
+    //Scope and Access Modifiers
+
+    /* All code has a scope, or a section where it can be used.
+     * Some examples of scope are private and public.
+     * Public variables and functions can be accessed by the class or object they are in, as well as other scripts.
+     * Private variables are only available to the code block they exist in.
+     * The block a variable exists in is the block where it is defined. If a variable is made in an if statement, for example,
+     * that variable will only be available while the if statement is executing.
+     */
+
+
+     //This public variable will always be available while this scrip is loaded, or in Unity's case, while an object containing this script exists.
+    public int freeNumber = 10;
+
+    //Unity can access public variables available to classes in the investigator, so they are very useful for variables such as the following:
+    public int speed;
+    public float pointsPerKill;
+    public Vector3 moveDirection;
+    //They don't even have to be initialized before use in some cases, because they can be given a setting in the investigator. But usually a default value is preferable.
+    //Unity overrides initialization values if the investigator is used.
+
+    //This variable can't be accessed outside this script
+    private int secretNumber = 0;
+
+    //Variables are private by default.
+    int otherSecretNumber = 5;
+
+
+	// Functions are also scoped. They are private by default. They can also be protected, which makes them public to children but private to everything else.
+	void Start () {
+
+        if (true)
+        {
+            //This variable can't be used outside the if statement, and is pretty useless.
+            int unusableNumber = 2;
+        }
+
+        //Variables can be made with the same name in different areas of code, as long as those blocks of code aren't in the same hierarchy.
+        //For instance, this one can't be initialized because then the one above conflicts with it.
+        //int unusableNumber = 3;
+
+	}
+	
+}
